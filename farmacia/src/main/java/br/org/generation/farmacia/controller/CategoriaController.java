63cd5f93cfa0b6973a9a075f87edc2d35c151003
Shiellyn Ferreira <brunashiellyn@gmail.com>
2021-11-09 10:21:00 -0300

    adicionando classe controller alterada

--- farmacia/src/main/java/br/org/generation/farmacia/controller/CategoriaController.java
+++ farmacia/src/main/java/br/org/generation/farmacia/controller/CategoriaController.java
@@ -2,11 +2,18 @@ package br.org.generation.farmacia.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +46,31 @@ public class CategoriaController {
 			return ResponseEntity.ok(categoriaRepository.findAllByCategoriaContainingIgnoreCase(categoria));
 		}
 		
+		@PostMapping
+		public ResponseEntity<Categoria> postCategoria(@RequestBody Categoria tipo){
+			return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(tipo));	
+		}
+		
+		@PutMapping
+		public ResponseEntity<Categoria> putCategoria(@Valid @RequestBody Categoria categoria) {
+						
+			return categoriaRepository.findById(categoria.getId())
+					.map(resposta -> {
+						return ResponseEntity.ok().body(categoriaRepository.save(categoria));
+					})
+					.orElse(ResponseEntity.notFound().build());
+
+		}
+
+		@DeleteMapping("/{id}")
+		public ResponseEntity<?> deleteCategoria(@PathVariable long id) {
+			
+			return categoriaRepository.findById(id)
+					.map(resposta -> {
+						categoriaRepository.deleteById(id);
+						return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+					})
+					.orElse(ResponseEntity.notFound().build());
+		}
 	}
 
